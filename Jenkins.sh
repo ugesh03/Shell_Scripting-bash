d6d184fa61f6a6d21e657aa9f79d1b1d721ab9e6
Ugesh <138633472+ugesh03@users.noreply.github.com>
2025-07-18 15:09:23 +0530

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# updating the package
+yum update
+
+# Install JDK-version=21 // yum install java-21-openjdk
+yum install java-21-*
+
+# install wget
+yum install wget -y
+
+## Add Jenkins repository
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+# Import GPG key
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# Install Jenkins
+sudo yum install jenkins -y
+
+# Enable and start the Jenkins service:
+systemctl enable jenkins
+systemctl start jenkins
